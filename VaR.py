bd032a0b088c1b5e8827577bf5f43f8eb9e40c3f
Utkarsh Choudhary <47511836+utkarshrutgers@users.noreply.github.com>
2019-03-29 20:35:11 -0400

    Update VaR.py

--- VaR.py
+++ VaR.py
@@ -6,7 +6,7 @@ def value_at_risk(returns, confidence_level=.05):
 	Parameters
 	----------
 	returns : pandas.DataFrame
-		Returns of each time serie. It could be daily, weekly, monthly, ...
+		Returns of each time series. It could be daily, weekly, monthly, ...
 		
 	confidence_level : int
 		Confidence level. 5% by default.
@@ -20,3 +20,30 @@ def value_at_risk(returns, confidence_level=.05):
 	
 	# Calculating VaR
 	return returns.quantile(confidence_level, axis=0, interpolation='higher')
+
+
+def expected_shortfall(returns, confidence_level=.05):
+	"""
+	It calculates the Expected Shortfall (ES) of some time series. It represents 
+	the average loss according to the Value at Risk.
+	
+	Parameters
+	----------
+	returns : pandas.DataFrame
+		Returns of each time serie. It could be daily, weekly, monthly, ...
+		
+	confidence_level : int
+		Confidence level. 5% by default.
+			
+	Returns
+	-------
+	es : pandas.Series
+		Expected Shortfall for each time series.
+	
+	"""
+	
+	# Calculating VaR
+	var = value_at_risk(returns, confidence_level)
+	
+	# ES is the average of the worst losses (under var)
+	return returns[returns.lt(var, axis=1)].mean()
